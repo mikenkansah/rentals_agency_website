bbf9da1d3807dd673ecf77e7f249e6ae0f6cda9a
Michael Kwame Nkansah <nkansahmichael232@gmail.com>
2023-01-07 22:46:06 +0000

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import "./App.css";
 import Hero from "./components/Hero";
+import hero from "./assets/hero.png";
 
 function App() {
   return (
     <div className="App">
-      <Hero />
+      <Hero hero={hero}/>
     </div>
   );
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,13 +1,22 @@
 import hero from "../assets/hero.png";
-
+import logo from "../assets/logo.png";
 
 const Hero = () => {
   return (
     <>
-      <section className="" style={{backgroundImage:`url(${hero})`, height:'100vh',backgroundRepeat:'no-repeat', backgroundSize:'cover'}}>
-  <div>
-
-  </div>
+      <section className={`bg-cover bg-no-repeat h-[100vh]`} style={{ backgroundImage: `url(${hero})` }}>
+        <div className="md:mx-[10rem]">
+          <nav className="flex  pt-4 ">
+            <img src={logo} alt="" className="inline" />
+            <ul className="flex  gap-10 ml-[30rem] text-white  text-2xl  ">
+              <li className="hover:underline hover:underline-offset-[22px] hover:decoration-[3px] z-[1] cursor-pointer" >Home</li>
+              <li className="hover:underline hover:underline-offset-[22px] hover:decoration-[3px] z-[1] cursor-pointer">Landlords</li>
+              <li  className="hover:underline hover:underline-offset-[22px] hover:decoration-[3px] z-[1] cursor-pointer">Tenants</li>
+              <li  className="hover:underline hover:underline-offset-[22px] hover:decoration-[3px] z-[1] cursor-pointer">Contact Us</li>
+            </ul>
+          </nav>
+          <hr className="brightness-75 mt-2"></hr>
+        </div>
       </section>
     </>
   );
