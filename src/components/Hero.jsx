6425456b5c6e2cab687d31ff047b3e52af8f8c60
Michael Kwame Nkansah <nkansahmichael232@gmail.com>
2023-01-07 21:06:25 +0000

    updates

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,13 @@
 import hero from "../assets/hero.png";
 
+
 const Hero = () => {
   return (
     <>
-      <section className="">
-        <h1>djidihsj0j</h1>
+      <section className="" style={{backgroundImage:`url(${hero})`, height:'100vh',backgroundRepeat:'no-repeat', backgroundSize:'cover'}}>
+  <div>
+
+  </div>
       </section>
     </>
   );
