df268df05a29d5f011faa57c0fdbaae50f31e4f4
Michael Kwame Nkansah <nkansahmichael232@gmail.com>
2023-01-08 14:27:19 +0000

    updates

--- src/components/MinimumCost.jsx
+++ src/components/MinimumCost.jsx
@@ -1,11 +1,17 @@
 const Minimum_Cost = () => {
-    return ( 
-        <>
-        <section className="mt-10">
-            <h1>Minimum</h1>
-        </section>
-        </>
-     );
-}
- 
+  return (
+    <>
+      <section className="my-10 md:mx-[10rem]">
+        <h1 className="text-4xl underline decoration-red-500 underline-offset-[10px] decoration-4">
+          Minimum Living Cost Takes Care Of Everything
+        </h1>
+
+        <div>
+            
+        </div>
+      </section>
+    </>
+  );
+};
+
 export default Minimum_Cost;
